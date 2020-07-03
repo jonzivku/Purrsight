74ecdcbc956ca72be263449fd2ab2a73cc62713b
jonzivku <jzivku@ucsc.edu>
2020-07-03 13:19:10 -0700

    user registration

--- src/purrsight/urls.py
+++ src/purrsight/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as user_views
 
 urlpatterns = [
+    path('', user_views.login_view, name='splash'),
     path('admin/', admin.site.urls),
+    path('register/', user_views.register_view, name='register'),
+    path('login/', user_views.login_view, name='login'),
+
 ]
