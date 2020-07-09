4cd40670175b9022936313666be69fa37b6d1aee
Jake Forrester <jake4ester@gmail.com>
2020-07-09 16:46:09 -0700

    removing old backend

--- backend/pages/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# View for Feed page
-def feed_view(request, *args, **kwargs):
-	return render(request, "pages/feed.html", {})
