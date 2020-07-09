05e2b23640abf646302b77c09d9672f5bb204eb6
hsliao <hsliao@ucsc.edu>
2020-07-08 21:01:22 -0700

    moved functions into seperate files

--- Reactjs/my-app/src/App.js
+++ Reactjs/my-app/src/App.js
@@ -1,126 +1,20 @@
 import React, { useState } from 'react';
-import {Row, Col, InputGroup, Jumbotron, Container, Button, Form, FormControl, Navbar, Nav, NavItem, MenuItem} from 'react-bootstrap';
+import {Jumbotron} from 'react-bootstrap';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {SignIn} from './SignIn.js'
+import {SignUp} from './SignUp.js'
+import {Profile} from './Profile.js'
+import {Home} from './Home.js'
+import {NavBar} from './NavBar.js'
+
 import './App.css';
-
-const NAV = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/Home">Home</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="justify-content-front">
-          <Nav.Link href="/Profile">Profile</Nav.Link>
-        </Nav>
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="justify-content-end">
-            <Nav.Link href="/SignUp">Sign Up</Nav.Link>
-          </Nav>
-          <Nav className="justify-content-end">
-            <Nav.Link href="/SignIN">Sign In</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
-
-function Home() {
-  return (
-    <div>
-      <h1>waaaah issa hommeeee</h1>
-    </div>  
-  );
-}
-
-function SignIn(){
-  return (
-    <div>
-      <Row>
-        <Col md={{ span: 4, offset: 4 }} fluid="md">
-          <Form>
-            <h2>Sign In</h2>
-            <Form.Group as={Row} controlId="formHorizontalEmail">
-              <Form.Label column sm={2}>
-                Email
-              </Form.Label>
-              <Col sm={10}>
-                <Form.Control type="email" placeholder="Email" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="formHorizontalPassword">
-              <Form.Label column sm={2}>
-                Password
-              </Form.Label>
-              <Col sm={10}>
-                <Form.Control type="password" placeholder="Password" />
-              </Col>
-            </Form.Group>
-          </Form>
-          <Button variant="primary" type="submit" >
-            Join
-          </Button>
-        </Col>
-      </Row>
-    </div>
-    )
-}
-
-function SignUp() {
-  return (
-    <div>
-      <Row>
-        <Col md={{ span: 4, offset: 4 }} fluid="md">
-          <Form>
-            <h2>Sign Up</h2>
-            <Form.Group as={Row} controlId="formHorizontalEmail">
-              <Form.Label column sm={2}>
-                Email
-              </Form.Label>
-              <Col sm={10}>
-                <Form.Control type="email" placeholder="Email" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="formHorizontalPassword">
-              <Form.Label column sm={2}>
-                Password
-              </Form.Label>
-              <Col sm={10}>
-                <Form.Control type="password" placeholder="8-12 characters" />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId="formHorizontalPassword">
-              <Form.Label column sm={2}>
-                Confirm Password
-              </Form.Label>
-              <Col sm={10}>
-                <Form.Control type="password" placeholder="Enter your password again" />
-              </Col>
-            </Form.Group>
-          </Form>
-          <Button variant="primary" type="submit" >
-            Join
-          </Button>
-        </Col>
-      </Row>
-    </div>
-  );
-}
-
-function Profile() {
-  return (
-    <div>
-      <h1>waaaah issa meeeeee</h1>
-    </div>
-  );
-}
 
 const App = () => (
   <>
     <Jumbotron>
       <h1 className="header">P U R R S I G H T</h1>
-      <NAV>
-      </NAV>
+      <NavBar>
+      </NavBar>
     </Jumbotron>
     <Router>
       <div>
---
+++ Reactjs/my-app/src/Profile.js
@@ -0,0 +1,10 @@
+//Profile.js
+import React from 'react';
+
+export function Profile() {
+  return (
+    <div>
+      <h1>THIS IS A PROFILE PAGE</h1>
+    </div>
+  );
+}
