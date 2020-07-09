917e58904b6dcac952d797f90d121e993bfb7134
Jake Forrester <jake4ester@gmail.com>
2020-07-09 16:50:32 -0700

    added new backend

---
+++ backendPurrsiteApi/posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Post(models.Model):
+    username = models.CharField(max_length=70, blank=False, default='')
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
+    imagesource = models.CharField(max_length=70, blank=False, default='')
---
+++ backendPurrsiteApi/posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+from posts.models import Post
+from posts.serializers import PostSerializer
+from rest_framework.decorators import api_view
+
+
+# Create your views here.
+@api_view(['POST'])
+def posts_list(request):
+    if request.method == 'POST':
+        post_data = JSONParser().parse(request)
+        post_serializer = PostSerializer(data=post_data)
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return JsonResponse(post_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
