4f9e2363b44da5cfcabafb1fe2853b4f99284c1b
jonzivku <jzivku@ucsc.edu>
2020-07-19 19:19:12 -0700

    picture upload.. maybe?

--- Reactjs/my-app/src/components/PostingCard.js
+++ Reactjs/my-app/src/components/PostingCard.js
@@ -5,11 +5,14 @@ import axios from 'axios';
 
 // Post localhost:8000/profile/posts/ { token, picture, description }
 export default class PostingCard extends React.Component {
-
-  state = {
-    token: null,
-    picture: null,
-    description: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: localStorage.getItem('token'),
+      picture: null,
+      description: null
+    }
+    console.log(this.state.token);
   }
 
   fileSelectedHandler = event => {
@@ -18,11 +21,24 @@ export default class PostingCard extends React.Component {
     })
   }
 
+  descriptionHandler = e => {
+    this.setState({
+      description: e
+    })
+  }
+
   fileUploadHandler = () => {
+    // need to check for null before we continue
     const fd = new FormData();
+
+    fd.append('token',
+      this.state.token)
     fd.append('picture',
       this.state.picture,
       this.state.picture.name)
+    fd.append('description',
+      this.state.description)
+
 
     axios.post('http://localhost:8000/profile/posts/', fd, {
       onUploadProgress: progressEvent => {
@@ -45,14 +61,21 @@ export default class PostingCard extends React.Component {
     // const { uploading, images } = this.state
 
     return(
-
       <Card>
         <Card.Header>
           <input type="file" onChange={this.fileSelectedHandler} />
         </Card.Header>
         <Card.Body>
-          <Form.Control as="textarea" rows="3" />
-          <Button variant="primary" onClick={this.fileUploadHandler}>
+          <Form.Control
+            as="textarea"
+            rows="3"
+            onChange={this.descriptionHandler}
+            />
+
+          <Button
+            variant="primary"
+            onClick={this.fileUploadHandler}
+            >
             Upload
           </Button>
         </Card.Body>
